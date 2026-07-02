8b137ae2c35aa292b72e22e6714cd75c6780d5e2
Nisarg Patel <pateln1810@gmail.com>
2026-07-02 20:04:59 +0530

    style: remove width restrictions on logo images in marquee so they scale to full height and aspect ratio

--- src/components/ClientLogos.tsx
+++ src/components/ClientLogos.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 
 export default function ClientLogos() {
   return (
@@ -15,31 +14,21 @@ export default function ClientLogos() {
         </div>
 
         {/* Carousel constrained to page width and margins */}
-        <div className="relative w-full overflow-hidden py-12 bg-white">
-          {/* Infinite scrolling wrapper */}
-          <div className="flex w-[200%] animate-scroll whitespace-nowrap gap-0">
-            <div className="w-1/2 flex justify-center items-center shrink-0">
-              <Image
-                src="https://macj-abuyerschoice.com/wp-content/uploads/2018/10/macjclientlist.jpg"
-                alt="MACJ Corporate Clients List"
-                width={6400}
-                height={600}
-                className="object-contain h-64 sm:h-96 w-auto select-none"
-                unoptimized
-                priority
-              />
-            </div>
-            <div className="w-1/2 flex justify-center items-center shrink-0">
-              <Image
-                src="https://macj-abuyerschoice.com/wp-content/uploads/2018/10/macjclientlist.jpg"
-                alt="MACJ Corporate Clients List Duplicate"
-                width={6400}
-                height={600}
-                className="object-contain h-64 sm:h-96 w-auto select-none"
-                unoptimized
-                priority
-              />
-            </div>
+        <div className="relative w-full overflow-hidden py-6 bg-white">
+          {/* Infinite scrolling wrapper using raw img to bypass flex-basis scaling constraints */}
+          <div className="flex w-max animate-scroll gap-0">
+            <img
+              src="https://macj-abuyerschoice.com/wp-content/uploads/2018/10/macjclientlist.jpg"
+              alt="MACJ Corporate Clients List"
+              className="h-20 md:h-28 w-auto max-w-none shrink-0 select-none"
+              draggable="false"
+            />
+            <img
+              src="https://macj-abuyerschoice.com/wp-content/uploads/2018/10/macjclientlist.jpg"
+              alt="MACJ Corporate Clients List Duplicate"
+              className="h-20 md:h-28 w-auto max-w-none shrink-0 select-none"
+              draggable="false"
+            />
           </div>
         </div>
 
