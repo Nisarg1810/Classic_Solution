57b356f5d26e6b8f54dd1cfdd86526b96cf88f0c
Nisarg Patel <pateln1810@gmail.com>
2026-07-02 20:02:32 +0530

    style: increase client logo sizes further to h-32 sm:h-48

--- src/components/ClientLogos.tsx
+++ src/components/ClientLogos.tsx
@@ -15,16 +15,16 @@ export default function ClientLogos() {
         </div>
 
         {/* Carousel constrained to page width and margins */}
-        <div className="relative w-full overflow-hidden py-4 bg-white">
+        <div className="relative w-full overflow-hidden py-8 bg-white">
           {/* Infinite scrolling wrapper */}
           <div className="flex w-[200%] animate-scroll whitespace-nowrap gap-0">
             <div className="w-1/2 flex justify-center items-center shrink-0">
               <Image
                 src="https://macj-abuyerschoice.com/wp-content/uploads/2018/10/macjclientlist.jpg"
                 alt="MACJ Corporate Clients List"
-                width={2400}
-                height={200}
-                className="object-contain h-24 sm:h-36 w-auto select-none"
+                width={3200}
+                height={300}
+                className="object-contain h-32 sm:h-48 w-auto select-none"
                 unoptimized
                 priority
               />
@@ -33,9 +33,9 @@ export default function ClientLogos() {
               <Image
                 src="https://macj-abuyerschoice.com/wp-content/uploads/2018/10/macjclientlist.jpg"
                 alt="MACJ Corporate Clients List Duplicate"
-                width={2400}
-                height={200}
-                className="object-contain h-24 sm:h-36 w-auto select-none"
+                width={3200}
+                height={300}
+                className="object-contain h-32 sm:h-48 w-auto select-none"
                 unoptimized
                 priority
               />
